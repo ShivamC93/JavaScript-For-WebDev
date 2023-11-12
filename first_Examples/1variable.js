453e2f7e9bc1c9f7cbd927586b2f6dbe4f106fd8
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2023-11-12 18:30:58 +0530

    Add files via upload

---
+++ first_Examples/1variable.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+const accountId = 124433;
+let accountEmail = "shivam@google.com";
+var accountPass = "1238";
+accountCity = "Pune";
+
+//To call single variable.
+console.log(accountId);
+
+// To call multiple variable at once in form of table.
+console.table([accountEmail, accountPass, accountCity])
