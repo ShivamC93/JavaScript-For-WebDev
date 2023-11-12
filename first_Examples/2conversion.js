0b3ee4fd276e535d8f22e8bd1b170e4947dda25a
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2023-11-12 18:30:38 +0530

    Add files via upload

---
+++ first_Examples/2conversion.js
@@ -0,0 +1,22 @@
+let name1 = "33"
+console.log(typeof(name1));
+
+let new1 = Number(name1);
+console.log(new1);
+console.log(typeof(new1));
+
+
+let name2 = 1;
+console.log(typeof(name2));
+
+let new2 = Boolean(name2);
+console.log(new2);
+console.log(typeof(new2));
+
+
+let name3 = 33;
+console.log(typeof(name3));
+
+let new3 = String(name3);
+console.log(new3);
+console.log(typeof(new3));
